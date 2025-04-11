023ce7b990d127f096760201f909260b26c603f2
Shahd Adel <shoadell9@gmail.com>
2025-04-11 14:47:18 +0200

    last api

--- lib/auth/domain/entities/AuthResultEntity .dart
+++
@@ -1,6 +0,0 @@
-import 'package:graduation_project/auth/domain/entities/RegisterResponseEntity%20.dart';
-
-class AuthResultEntity {
-  RegisterResponseEntity? registerResponseEntity;
-  AuthResultEntity({this.registerResponseEntity});
-}
